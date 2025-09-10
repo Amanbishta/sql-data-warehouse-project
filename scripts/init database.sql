4b0ffafbd18cf4a7178d946dff2c04966faddd7f
Amanbishta <160509290+Amanbishta@users.noreply.github.com>
2025-09-10 21:26:49 +0530

    Create init database.sql

---
+++ scripts/init database.sql
@@ -0,0 +1,12 @@
+-- create database "Data_Warehouse"
+use master;
+ create database DataWarehouse;
+
+ use DataWarehouse;
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
