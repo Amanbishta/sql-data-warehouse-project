ad2b5c39f21f0fee3bc3e1156058e90f7d9fa57f
Amanbishta <160509290+Amanbishta@users.noreply.github.com>
2025-09-11 10:26:46 +0530

    Update init database.sql

--- scripts/init database.sql
+++ scripts/init database.sql
@@ -1,3 +1,8 @@
+ -- with in database bronze silver gold
+WARNING ⚠️ 
+ -- Running this script will drop the entire 'Datawarehouse' database if it exists.
+  --All data in the database will be permanently deleted. Proceed with caution
+   -- and ensure you have proper backups before running this script
 -- create database "Data_Warehouse"
 use master;
  create database DataWarehouse;
