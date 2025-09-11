4fd31df23ae59734669f450a04dc9e50df4449db
Amanbishta <160509290+Amanbishta@users.noreply.github.com>
2025-09-11 10:52:28 +0530

    Update init_database.sql

--- scripts/init database.sql
+++ scripts/init database.sql
@@ -1,4 +1,11 @@
- -- with in database bronze silver gold
+/* 
+=====================================================================
+Creata Database and Sehemas
+=====================================================================
+Script Purpose;
+  This scripts create a new database named 'DataWarehouse' after checking if it already exists.
+  If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+  with in database bronze silver gold
 WARNING ⚠️ 
  -- Running this script will drop the entire 'Datawarehouse' database if it exists.
   --All data in the database will be permanently deleted. Proceed with caution
